15321238e7024a90bfffd17a74378a041b4cb75e
“andreiantonescu” <andrei.antonescu1@gmail.com>
2023-09-22 01:43:18 +0300

    added some css, deterministic random seed

--- week1/README-week1.md
+++ week1/README-week1.md
@@ -1,4 +1,2 @@
 - literal exploration of the Musical Web
 
-Credits:
-- https://freesound.org/people/wtermini/sounds/546450/
--- week1/script.js
+++ week1/script.js
@@ -1,13 +1,36 @@
 
-let srcLinks
-let audioElNo = 50
-let audioElements = []
-let couldFetch = false
-let speed = 1000
-let isPlaying = false
-let audioElLoadedNo = 0
-let loadingElem, websiteURL, resultElem
-const files = ['/week1/audio/188828__0ktober__modem_dial.wav', '/week1/audio/546450__wtermini__the-sound-of-dial-up-internet.mp3', '/week1/audio/644997__goldenzoomi1__the-microsoft-sound.wav']
+let srcLinks, audioElNo = 50, audioElLoadedNo = 0, audioElements = []
+let couldFetch = false, speed = 1000, isPlaying = false
+let loadingElem, websiteURL, resultElem, seed = 123512
+
+const files = [
+    '/week1/audio/188828__0ktober__modem_dial.wav', 
+    '/week1/audio/546450__wtermini__the-sound-of-dial-up-internet.mp3', 
+    '/week1/audio/644997__goldenzoomi1__the-microsoft-sound.wav']
+
+S = () => { return (1664525 * seed + 1013904223) % (2 ** 32) / (2 ** 32)}
+
+(function() {
+    // Parameters for a simple linear congruential generator
+    const a = 1664525;
+    const c = 1013904223;
+    const m = 2 ** 32; // modulus: 2^32
+
+    let seed = 12345678
+
+    function lcgRandom() {
+        seed = (a * seed + c) % m;
+        return seed / m;
+    }
+
+    // Override Math.random() with the seedable lcgRandom
+    Math.random = lcgRandom;
+
+    // Function to set the seed
+    Math.setSeed = function(s) {
+        seed = s;
+    };
+})()
 
 async function extractSrcLinks() {
     try {
@@ -15,49 +38,51 @@ async function extractSrcLinks() {
             headers: {
                 'X-Requested-With': 'XMLHttpRequest'
             }
-        });
+        })
 
         if (!response.ok) {
             couldFetch = false
-            throw new Error("Ooops, couldn't fetch")
+            throw new Error("Ooops, couldn't fetch 🙅🏼")
         }
 
         const htmlContent = await response.text()
 
         const parser = new DOMParser();
         const doc = parser.parseFromString(htmlContent, 'text/html')
-        // Extract all src attribute
+
         const srcTags = doc.querySelectorAll('[src]')
         srcLinks = [...srcTags].map(tag => {
             return tag.getAttribute('src')
         })
 
-        // Display the links
-        resultElem.textContent = srcLinks;
+        resultElem.innerHTML = srcLinks.map(link => `<a href="${link}">${link}</a>`).join('<br>')
 
         if(srcLinks != undefined && srcLinks.length > 1) {
             audioElements = []
+            Math.setSeed(srcLinks.length * srcTags.length)
             for(let i=0; i<audioElNo; i++) {
                 createAudioElement(i)
             }
             couldFetch = true
+        } else {
+            throw new Error("Nothing found 😟")
         }
     } catch (error) {
         couldFetch = false
-        resultElem.textContent = "Ooops, couldn't fetch"
+        resultElem.textContent = error.message
     }
 }
 
 function createAudioElement(index) {
     let audio = document.createElement('audio')
     audio.src = files[Math.floor(Math.random() * files.length)]
-    audio.controls = true
-    audio.volume = 1/audioElNo
+    // audio.controls = true
+    audio.volume = 2.5/audioElNo
     audio.loop = true
     document.body.appendChild(audio)
     audioElements.push({
         audioEl: audio,
-        src: srcLinks[Math.floor(Math.random() * (srcLinks.length - 1))],
+        link: srcLinks[Math.floor(Math.random() * (srcLinks.length - 1))],
         currentIndex: 0
     })
 }
@@ -70,12 +95,19 @@ window.onload = function() {
     loadingElem = document.getElementById('loading')
     websiteURL = document.getElementById('websiteURL')
     resultElem = document.getElementById('result')
+    Math.setSeed(12345678)
     setInterval(update, speed);
   };
 
 
 function update() {
+    console.log("update")
     if (couldFetch == false || isPlaying == false) {
+        for(let i=0; i<audioElNo; i++) {
+            if (audioElements[i] != undefined) {
+                audioElements[i].audioEl.pause()
+            }
+        }
         return
     }
     audioElLoadedNo = 0
@@ -83,21 +115,21 @@ function update() {
         if(audioElements[i].audioEl.readyState != 4) {
             continue
         }
-        if(audioElements[i].currentIndex >= audioElements[i].src.length) {
+        if(audioElements[i].currentIndex >= audioElements[i].link.length) {
             audioElements[i].currentIndex = 0
         }
-        playSegment(audioElements[i].audioEl, audioElements[i].src[audioElements[i].currentIndex])
+        playSegment(audioElements[i].audioEl, audioElements[i].link[audioElements[i].currentIndex])
         audioElements[i].currentIndex++
         audioElLoadedNo++;
     }
-    resultElem.textContent = audioElLoadedNo/audioElNo * 100 + "% loaded"
+    loadingElem.textContent = audioElLoadedNo/audioElNo * 100 + "% loaded"
 }
 
 function playSegment(audioEl, character) {
-    const segmentDuration = audioEl.duration / 26;
-    const charPosition = character.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0);  // 0 for 'a', 1 for 'b', etc.
+    const segmentDuration = audioEl.duration / 26
+    const charPosition = character.toLowerCase().charCodeAt(0) - 'a'.charCodeAt(0)
 
-    if (charPosition >= 0 && charPosition <= 25 && Math.random()>0.5) {
+    if (charPosition >= 0 && charPosition <= 25 && Math.random() > 0.5) {
         const startTime = charPosition * segmentDuration
     
         audioEl.currentTime = startTime
