1bbabaa1f35ae6a18cc2cf3ac9f9fc6f95e51107
“andreiantonescu” <andrei.antonescu1@gmail.com>
2023-11-09 00:03:19 +0200

    room and window design

--- final/script.js
+++ final/script.js
@@ -19,68 +19,80 @@ function init() {
   renderer.setAnimationLoop( animation );
   document.body.appendChild( renderer.domElement );
 
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-  camera.position.set( 0, 5, - 15 );
-  camera.lookAt(- 5, 5, 150)
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 )
+  camera.position.set(0, 1, -1)
 
-  scene = new THREE.Scene();
-
-  RectAreaLightUniformsLib.init();
-
-  const rectLight1 = new THREE.RectAreaLight( 0xfff5ff, 1, 5, 50 )
-  rectLight1.position.set(- 5, 5, 150)
-  scene.add( rectLight1 )
-
-  scene.add( new RectAreaLightHelper( rectLight1 ) );
+  scene = new THREE.Scene()
 
   const geoFloor = new THREE.BoxGeometry( 2000, 0.1, 2000 );
-  const matStdFloor = new THREE.MeshStandardMaterial( { color: 0xbcbcbc, roughness: 0.25, metalness: 0 } );
+  const matStdFloor = new THREE.MeshBasicMaterial( { color: 0xf012aa, roughness: 0.25, metalness: 0 } );
   const mshStdFloor = new THREE.Mesh( geoFloor, matStdFloor );
 
-  scene.add( mshStdFloor );
+  // scene.add( mshStdFloor );
 
+  // helpers
+  const axesHelper = new THREE.AxesHelper(50)
+  scene.add(axesHelper)
+
+  const size = 50
+  const divisions = 100
+  const gridHelper = new THREE.GridHelper(size, divisions)
+  scene.add(gridHelper)
+
+
+  // add ROOM
   const materialArray = [
-    new THREE.MeshPhongMaterial( { color: 0xffbcbc, side: THREE.BackSide  } ), // Left wall
-    new THREE.MeshPhongMaterial( { color: 0xffbcbc, side: THREE.BackSide  } ), // Right wall
-    new THREE.MeshPhongMaterial( { color: 0xffbcbc, side: THREE.BackSide  } ), // Top wall
-    new THREE.MeshPhongMaterial( { color: 0xffbcbc, side: THREE.BackSide  } ), // Bottom wall
-    new THREE.MeshPhongMaterial( { color: 0xffbcbc, side: THREE.BackSide  } ), // Front wall
-    new THREE.MeshPhongMaterial( { color: 0xffbcbc, side: THREE.BackSide  } ), // Back wall
+    new THREE.MeshBasicMaterial( { color: 0xffbcbc, side: THREE.BackSide  } ), // Left wall
+    new THREE.MeshBasicMaterial( { color: 0xffbcbc, side: THREE.BackSide  } ), // Right wall
+    new THREE.MeshBasicMaterial( { color: 0xbbbcbc, side: THREE.BackSide  } ), // Top wall
+    new THREE.MeshBasicMaterial( { color: 0x222222, side: THREE.BackSide  } ), // Bottom wall
+    new THREE.MeshBasicMaterial( { color: 0xcccccc, side: THREE.BackSide  } ), // Front wall
+    new THREE.MeshBasicMaterial( { color: 0xaaaaaa, side: THREE.BackSide  } ), // Back wall
   ];
 
-  const directionalLight = new THREE.SpotLight(0xffffff, 1000);
+  const roomSize = 15
+  const roomHeight = 4
+  const directionalLight = new THREE.SpotLight(0xffffff);
   directionalLight.position.set(- 5, 5, 150); // Adjust the position accordingly
   directionalLight.castShadow = true
-  scene.add(directionalLight);
-  const roomGeometry = new THREE.BoxGeometry(250, 250, 500);
+  scene.add(directionalLight)
+
+  const roomWidth = roomSize * Math.random() + roomSize/2
+  const roomDepth = roomSize * Math.random() + roomSize
+  const roomGeometry = new THREE.BoxGeometry(roomWidth, roomHeight, roomDepth);
   // Create the room mesh
   const roomMesh = new THREE.Mesh(roomGeometry, materialArray);
-  roomMesh.position.set(0, 50, 10);
-  scene.add(roomMesh);
+  roomMesh.position.set(0, 0, 0);
+  scene.add(roomMesh)
 
-  const controls = new OrbitControls( camera, renderer.domElement );
-  controls.update();
-  
+  // add WINDOW
+  const windowWidth = roomWidth * Math.random()
+  const windowHeight = roomHeight * Math.random()
+  RectAreaLightUniformsLib.init()
+  const rectLight1 = new THREE.RectAreaLight( 0xfff5ff, 1, windowWidth > 1 ? windowWidth : 1, windowHeight > 1 ? windowHeight : 1)
+  rectLight1.position.set(0, 0, roomDepth/2 - 0.1)
+  scene.add( rectLight1 )
+  scene.add( new RectAreaLightHelper(rectLight1))
 
-  window.addEventListener( 'resize', onWindowResize );
+  const controls = new OrbitControls( camera, renderer.domElement )
+  controls.target.set(0, 0, 0)
+
+
+  window.addEventListener( 'resize', onWindowResize )
 
   stats = new Stats();
-  document.body.appendChild( stats.dom );
+  document.body.appendChild( stats.dom )
 
+  camera.lookAt(rectLight1.position)
 }
 
 function onWindowResize() {
-
   renderer.setSize( window.innerWidth, window.innerHeight );
   camera.aspect = ( window.innerWidth / window.innerHeight );
   camera.updateProjectionMatrix();
-
 }
 
 function animation( time ) {
-
-  renderer.render( scene, camera );
-
-  stats.update();
-
+  renderer.render( scene, camera )
+  stats.update()
 }
